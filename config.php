fd128827df60514722bd1a51ec6f7ef929cdbb5f
Robin Singh <robin.flyhigh@gmail.com>
2013-03-04 00:17:09 +0530

    config fix

--- config.php
+++ config.php
@@ -1,15 +1,18 @@
 <?php
+
+	error_reporting(E_ALL);
+
 	unset($CFG);
 
 	$CFG = new stdClass();
 
-	$CFG->dbhost		= 'localhost';
+	$CFG->dbhost		= 'http://robin-flyhigh-mini-twitter.herokuapp.com';
 	$CFG->dbusername	= 'b71737066fc0bf';
 	$CFG->dbpassword	= '5637a063';
 	$CFG->dbname		= 'heroku_9ea0cd79e460055';
 
 	$CFG->siteroot 		= 'http://robin-flyhigh-mini-twitter.herokuapp.com/';
-	$CFG->dataroot 		= 'E:/xampp/htdocs/PMS/minitwitter/uploads';
+	$CFG->dataroot 		= '/app/www/uploads';
 
 	require 'rb.php';	
 	R::setup("mysql:host=$CFG->dbhost;dbname=$CFG->dbname","$CFG->dbusername","$CFG->dbpassword");
